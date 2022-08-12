bbbb4803a3620bb661ef418603314fc03c42ad40
Voldemar <vladucha01@gmail.com>
2022-08-12 19:25:08 +0300

    add db

--- SqlWrapper/Program.cs
+++ SqlWrapper/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Rea
+﻿var content = await GetContentFromInputFile();
+
+if (string.IsNullOrEmpty(content))
+{
+    Console.WriteLine("File's content is null or empty");
+}
+
+
+
+Console.WriteLine("Enter any key to exit.");
+Console.ReadKey();
+
+async Task<string> GetContentFromInputFile()
+{
+    Console.Write("Enter input file path: ");
+    var path = Console.ReadLine();
+
+    if (File.Exists(path))
+    {
+        return await File.ReadAllTextAsync(path);
+    }
+    else
+    {
+        Console.WriteLine("File not found.");
+        return String.Empty;
+    }
+}
